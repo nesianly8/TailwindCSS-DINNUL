3744277db044916b124b884ac170a7483eca052d
nesianly8 <nesianly@gmail.com>
2024-03-07 17:03:13 +0700

    kelima commit

---
+++ mytailwind/tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./resources/**/*.blade.php",
+    "./resources/**/*.js",
+    "./resources/**/*.vue",
+    "./node_modules/flowbite/**/*.js"
+  ],
+  darkMode: 'class',
+  theme: {
+    container: {
+      center: true,
+      padding: '16px',
+    },
+    extend: {
+      colors: {
+        primary: '#ec4899',
+        secondary: '#64748b',
+        dark: '#0f172a',
+      },
+      screens: {
+        '2xl': '1320px',
+      },
+    },
+  },
+  plugins: [
+      require('flowbite/plugin')
+  ],
+}
+
